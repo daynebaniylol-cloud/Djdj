c3c5e11c3e1ce1c61db54972d981d32f3f1323c9
daynebaniylol-cloud <daynebaniylol@gmail.com>
2026-03-15 17:02:37 +0200

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -1,40 +1,48 @@
 import asyncio
 import os
 from pywebio import start_server
-from pywebio.input import input, TEXT
+from pywebio.input import input, TEXT, select
 from pywebio.output import put_text, put_scrollable, put_markdown, output, put_buttons
-from pywebio.session import run_async, run_js
+from pywebio.session import run_async, run_js, info as session_info
 
-chat_msgs = []
+# Храним пользователей: {никнейм: {'msg_box': объект_вывода, 'queue': очередь_сообщений}}
+users = {}
 
 async def main():
-    put_markdown("## 🧊 Чат запущен")
-    msg_box = output()
-    put_scrollable(msg_box, height=300, keep_bottom=True)
+    put_markdown("## 🧊 Telegram-style Chat")
+    
+    nickname = await input("Твой ник:", required=True)
+    if nickname in users:
+        put_text("Этот ник занят!")
+        return
 
-    # ОБЯЗАТЕЛЬНО await перед input
-    nickname = await input("Введите ник:", type=TEXT, required=True)
-    msg_box.append(put_markdown(f'📢 **{nickname}** зашел в чат'))
+    # Создаем область для вывода сообщений конкретно для этого юзера
+    user_msg_box = output()
+    users[nickname] = {'msg_box': user_msg_box, 'queue': asyncio.Queue()}
+    
+    put_scrollable(user_msg_box, height=300, keep_bottom=True)
+    put_text(f"Привет, {nickname}! Выбери, кому писать.")
 
-    refresh_task = run_async(refresh_msg(nickname, msg_box))
+    # Фоновая задача для проверки новых сообщений
+    run_async(listen_to_queue(nickname, user_msg_box))
 
     while True:
-        # ОБЯЗАТЕЛЬНО await перед input
-        data = await input("Сообщение:", type=TEXT)
-        msg_box.append(put_markdown(f"**{nickname}**: {data}"))
-        chat_msgs.append((nickname, data))
+        target = await select("Кому:", options=["Общий чат", *[n for n in users if n != nickname]])
+        msg = await input("Сообщение:")
 
-async def refresh_msg(nickname, msg_box):
-    last_idx = len(chat_msgs)
+        if target == "Общий чат":
+            for n, data in users.items():
+                await data['queue'].put(f"[{nickname} -> Всем]: {msg}")
+        else:
+            await users[target]['queue'].put(f"[{nickname} (ЛИЧКА)]: {msg}")
+
+async def listen_to_queue(nickname, user_msg_box):
+    """Слушает очередь сообщений пользователя"""
     while True:
-        await asyncio.sleep(1)
-        for m in chat_msgs[last_idx:]:
-            if m[0] != nickname:
-                msg_box.append(put_markdown(f"**{m[0]}**: {m[1]}"))
-        last_idx = len(chat_msgs)
+        msg = await users[nickname]['queue'].get()
+        user_msg_box.append(put_text(msg))
 
 if __name__ == "__main__":
-    # Порт для Render
     port = int(os.environ.get("PORT", 8080))
     start_server(main, port=port, host='0.0.0.0', cdn=False)
     
